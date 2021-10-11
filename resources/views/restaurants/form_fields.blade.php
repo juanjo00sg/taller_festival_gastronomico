0db3fd17a073c30350b905274fa7a91308b22f70
juanjo00sg <jujo00sg@gmail.com>
2021-10-10 20:30:21 -0500

    Actualización de formulario en Restaurantes para campos faltantes

--- resources/views/restaurants/form_fields.blade.php
+++ resources/views/restaurants/form_fields.blade.php
@@ -12,13 +12,45 @@
             </div>
             <div class="mb">
                 {{ Form::label('phone', 'Teléfono', ['class' => 'form-label']) }}
-                {{ Form::text('phone', null, ['class' => 'form-control', 'maxlength' => 10]) }}
+                {{ Form::number('phone', null, ['class' => 'form-control', 'maxlength' => 10]) }}
             </div>
             <div class="mb">
                 {{ Form::label('delivery', '¿Tiene domicilio?', ['class' => 'form-label']) }}
-                {{ Form::select('delivery', ['y' => 'Si', 'n' => 'No'], null, ['class' => 'form-control']); }}
+                {{ Form::select('delivery', ['y' => 'Si', 'n' => 'No'], null, ['class' => 'form-control']) }}
             </div>
             <div class="mb mb-3">
                 {{ Form::label('category_id', 'Categoría', ['class' => 'form-label']) }}
-                {{ Form::select('category_id', $categories, null, ['class' => 'form-control']); }}
+                {{ Form::select('category_id', $categories, null, ['class' => 'form-control']) }}
+            </div>
+            <div class="mb mb-3">
+                {{ Form::label('schedule', 'Horario', ['class' => 'form-label']) }}
+                {{ Form::text('schedule', null, ['class' => 'form-control']) }}
+            </div>
+            <div class=" mb mb-3">
+                {{ Form::label('latitude', 'Latidud', ['class' => 'form-label']) }}
+                {{ Form::text('latitude', null, ['class' => 'form-control', 'step' => 'any']) }}
+            </div>
+            <div class=" mb mb-3">
+                {{ Form::label('longitude', 'Longitud', ['class' => 'form-label']) }}
+                {{ Form::text('longitude', null, ['class' => 'form-control', 'step' => 'any']) }}
+            </div>
+            <div class="mb-3">
+                {{ Form::label('logo', 'Logo', ['class' => 'form-label']) }}
+                {{ Form::file('image', ['class' => 'form-control', 'accept' => 'image/jpeg']) }}                 
+            </div>
+            <div class="mb mb-3">
+                {{ Form::label('facebook', 'Facebook', ['class' => 'form-label']) }}
+                {{ Form::text('facebook', null, ['class' => 'form-control']) }}
+            </div>
+            <div class="mb mb-3">
+                {{ Form::label('twitter', 'Twitter', ['class' => 'form-label']) }}
+                {{ Form::text('twitter', null, ['class' => 'form-control']) }}
+            </div>
+            <div class="mb mb-3">
+                {{ Form::label('instagram', 'Instagram', ['class' => 'form-label']) }}
+                {{ Form::text('instagram', null, ['class' => 'form-control']) }}
+            </div>
+            <div class="mb mb-3">
+                {{ Form::label('youtube', 'Youtube', ['class' => 'form-label']) }}
+                {{ Form::text('youtube', null, ['class' => 'form-control']) }}
             </div>
