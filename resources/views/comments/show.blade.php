32674203847b9b8e825ccefea52312991a46f76a
juanjo00sg <jujo00sg@gmail.com>
2021-10-16 17:34:55 -0500

    Mejora de visualización de los comentarios

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -5,30 +5,40 @@
     <div class="container">
         <h1>Comentarios del restaurante</h1>
 
-
-
         @foreach ($comments as $comm)
-            <div class="row">
-                <div class="col-sm-2">
-                <label for="">Comentario:</label>
-            </div>
-            <div class="col-md-4">
-                <label for="">{{ $comm->comment }}</label>
-            </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-2">
-                    <label for="">Puntaje:</label>
+            <div class="jumbotron">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label for="">Usuario:</label>
+                    </div>
+                    <div class="col-md-4">                                             
+                        <label for="">{{ $comm->user->name }}</label>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <label for="">{{ $comm->score }}</label>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label for="">Comentario:</label>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="">{{ $comm->comment }}</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label for="">Puntaje:</label>
+                    </div>
+                    <div class="col-md-4">
+                        @for ($i = 0; $i < $comm->score; $i++)
+                            <i class="fas fa-star"></i>
+                        @endfor
+                        <label for=""></label>
 
+                    </div>
                 </div>
             </div>
+
         @endforeach
 
-
-        {{-- {{ $restaurants->links() }} --}}
     </div>
 
 @endsection
